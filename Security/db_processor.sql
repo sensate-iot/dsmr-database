ec0724632e11c422749dd3e708c8ded75adb06bb
Michel Megens <michel@michelmegens.net>
2021-03-24 19:32:06 +0100

    db: import databasae project
    Import the DSMR database project from the dsmr-processor solution.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

---
+++ Security/db_processor.sql
@@ -0,0 +1,17 @@
+﻿CREATE ROLE [db_processor]
+GO
+
+GRANT EXECUTE ON [dbo].[DsmrProcessor_BulkInsertDataPoints] TO [db_processor]
+GO
+GRANT EXECUTE ON [dbo].[DsmrProcessor_FinalizeBatch] TO [db_processor]
+GO
+GRANT EXECUTE ON [dbo].[DsmrProcessor_InsertDataPoint] TO [db_processor]
+GO
+GRANT EXECUTE ON [dbo].[DsmrProcessor_InsertProcessingTimestamp] TO [db_processor]
+GO
+GRANT EXECUTE ON [dbo].[DsmrProcessor_SelectAllSensorMappings] TO [db_processor]
+GO
+GRANT EXECUTE ON [dbo].[DsmrProcessor_SelectLastProcessedBySensorId] TO [db_processor]
+GO
+GRANT EXECUTE ON [dbo].[DsmrProcessor_SelectSensorMapping] TO [db_processor]
+GO
