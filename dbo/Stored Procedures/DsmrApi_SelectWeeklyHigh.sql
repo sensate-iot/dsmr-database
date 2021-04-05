fe0ba6c95a78dbb81d7a5edd28153955d24d5f57
Michel Megens <michel@michelmegens.net>
2021-04-05 12:14:57 +0200

    dsmr-api: update the gas usage column name
    Select the gas flow property as `GasFlow' instead of `GasUage'.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- dbo/Stored Procedures/DsmrApi_SelectWeeklyHigh.sql
+++ dbo/Stored Procedures/DsmrApi_SelectWeeklyHigh.sql
@@ -4,10 +4,10 @@ AS
 BEGIN 
 	SELECT MAX([PowerUsage]) AS [PowerUsage],
 		   MAX([PowerProduction]) AS [PowerProduction],
-		   MAX([GasFlow]) * 1000.0 AS [GasUsage], -- In liters/minute
+		   MAX([GasFlow]) * 1000.0 AS [GasFlow], -- In liters/minute
 		   MAX([Temperature]) AS [Temperature],
 		   MAX([OutsideAirTemperature]) AS [Oat]
 	  FROM [dbo].[DataPoints]
-	  WHERE [SensorId] = 1
+	  WHERE [SensorId] = @sensorId
 		AND [Timestamp] >= DATEADD(DAY, -7, GETUTCDATE())
 END
