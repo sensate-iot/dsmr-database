dfcd366b7e30c5d9bb41b4e1c1a1bb146828dda6
Michel Megens <michel@michelmegens.net>
2021-06-03 19:47:24 +0200

    procedure: add DsmrApi_SelectEnergyDataBetween
    Add a stored procedure to compute the energy usage and energy production
    between two given dates for a given sensor (ID).
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

---
+++ dbo/Stored Procedures/DsmrApi_SelectEnergyDataBetween.sql
@@ -0,0 +1,30 @@
+﻿CREATE PROCEDURE [dbo].[DsmrApi_SelectEnergyDataBetween]
+	@sensorId INT,
+	@start DATETIME,
+	@end   DATETIME
+AS
+BEGIN
+	DECLARE @minEnergyUsage INT;
+	DECLARE @minEnergyProduction INT;
+
+	BEGIN TRANSACTION
+
+	SELECT TOP(1) @minEnergyUsage = [EnergyUsage]
+				 ,@minEnergyProduction = [EnergyProduction]
+	FROM [dbo].[DataPoints]
+	WHERE [Timestamp] >= @start
+	  AND [Timestamp] <  @end
+	  AND [SensorId] = @sensorId
+	ORDER BY [Timestamp] ASC
+
+
+	SELECT TOP(1) [EnergyUsage] - @minEnergyUsage AS [EnergyUsage]
+				 ,[EnergyProduction] - @minEnergyProduction AS [EnergyProduction]
+	FROM [dbo].[DataPoints]
+	WHERE [Timestamp] >= @start
+	  AND [Timestamp] <  @end
+	  AND [SensorId] = @sensorId
+	ORDER BY [Timestamp] DESC
+
+	COMMIT TRANSACTION
+END
