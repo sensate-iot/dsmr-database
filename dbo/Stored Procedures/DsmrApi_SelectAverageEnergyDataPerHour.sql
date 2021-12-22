f4a3948598c52acab2ce8bb45f95536d241cc502
Michel Megens <michel@michelmegens.net>
2021-12-22 23:11:31 +0100

    api: implement two additional sprocs
    Implement the following stored procedures for the DSMR API:
    
    - DsmrApi_ComputeDeviceCapabilities.sql;
    - DsmrApi_SelectAverageEnergyDataPerHour.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

---
+++ dbo/Stored Procedures/DsmrApi_SelectAverageEnergyDataPerHour.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[DsmrApi_SelectAverageEnergyDataPerHour]
+	@sensorId INT,
+	@start DATETIME,
+	@end DATETIME
+AS
+BEGIN
+	SELECT DATEPART(HOUR, [Timestamp]) as [Hour],
+		   AVG([PowerUsage]) AS [AveragePowerUsage],
+		   AVG([PowerProduction]) AS [AveragePowerProduction],
+		   AVG([GasFlow]) * 60 * 1000 AS [AverageGasFlow] -- Convert gas flow to L/min
+	FROM [dbo].[DataPoints]
+	WHERE [SensorId] = @sensorId
+	  AND [Timestamp] >= @start
+	  AND [Timestamp] < @end
+	GROUP BY [SensorId],
+			 DATEPART(HOUR, [Timestamp])
+	ORDER BY [Hour]
+END
